157cf9c4227243927e9e25ea2eaddecc2126e1c6
Jose Dario Sanchez <josedario.xyz@gmail.com>
2018-06-28 10:34:11 -0700

    adds jest mocks for testing + code style standardization

--- client/src/containers/App/App.test.js
+++ client/src/containers/App/App.test.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
-it('fetches candidates');
-it('patches candidates');
+import App from "./App";
 
-it('filters candidates by all');
-it('filters candidates by reviewed');
-it('filters candidates by unreviewed');
-it('sorts candidates by default');
-it('sorts candidates by statusNormal');
-it('sorts candidates by statusReverse');
-it('sorts candidates by dateAppliedNormal');
-it('sorts candidates by dateAppliedReverse');
+jest.mock("axios");
+jest.mock("./App");
+
+it("fetches candidates");
+it("patches candidates");
+
+it("filters candidates by all");
+it("filters candidates by reviewed");
+it("filters candidates by unreviewed");
+it("sorts candidates by default");
+it("sorts candidates by statusNormal");
+it("sorts candidates by statusReverse");
+it("sorts candidates by dateAppliedNormal");
+it("sorts candidates by dateAppliedReverse");
